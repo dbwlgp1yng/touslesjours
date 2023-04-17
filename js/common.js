e04ddda35cab4c9b97d56549d012f011561915de
dbwlgp1yng <dbwlgp1yng@gmail.com>
2023-04-18 00:14:47 +0900

    click event 수정

--- js/common.js
+++ js/common.js
@@ -15,8 +15,20 @@ $(document).ready(function() {
 $(document).ready(function() {
     var dropMenu = '.login_icon';
     var speed = 'fast';
+    var flag = 'off';
 
     $(dropMenu).find('.person_icon').click(function() {
         $(this).next().stop().slideToggle(speed);
+        if(flag == 'off') { // 클릭이 안됐을 때
+            $(this).css({
+                backgroundPosition: '-30px -30px'
+            });
+            flag = 'on';
+        } else if(flag == 'on') { // 클릭이 됐을 때
+            $(this).css({
+                backgroundPosition: '-30px 0'
+            });
+            flag = 'off';
+        }
     });
 });
