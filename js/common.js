94a9234c81bf5f767e3a26a921235cb8ad557f79
dbwlgp1yng <dbwlgp1yng@gmail.com>
2023-04-19 22:40:57 +0900

    add main slider and modify scrolltop_btn & login

--- js/common.js
+++ js/common.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     });
 });
 
+
 // login_icon click
 $(document).ready(function() {
     var dropMenu = '.login_icon';
@@ -20,15 +21,61 @@ $(document).ready(function() {
     $(dropMenu).find('.person_icon').click(function() {
         $(this).next().stop().slideToggle(speed);
         if(flag == 'off') { // 클릭이 안됐을 때
-            $(this).css({
-                backgroundPosition: '-30px -30px'
-            });
+            $(this).css('backgroundPosition', '-30px -30px');
             flag = 'on';
         } else if(flag == 'on') { // 클릭이 됐을 때
-            $(this).css({
-                backgroundPosition: '-30px 0'
-            });
+            $(this).css('backgroundPosition', '-30px 0');
             flag = 'off';
         }
     });
+    $(dropMenu).hover(function() {
+        $(this).find('.person_icon').stop().css('backgroundPosition', '-30px -30px');
+    });
+    $(dropMenu).mouseleave(function() {
+        $(this).find('ul').slideUp(speed);
+        $(this).find('.person_icon').stop().css('backgroundPosition', '-30px 0');
+        flag = 'off';
+    });
+    
+    $(dropMenu).find('.login_active a:last').keydown(function(e) {        
+        if(e.keyCode == 9) {  
+            if(!e.shiftKey) { 
+                $(dropMenu).trigger('mouseleave');
+            }
+        }
+    });
 });
+
+
+// scroll_top event
+$(document).ready(function(){
+    var scrollBtn = '.scroll_top a';
+    var speed = 1700;
+    
+    $(window).scroll(function(){
+      var sTop = $(this).scrollTop();
+     
+        if(sTop > 200){
+            $(scrollBtn).parent().stop().fadeIn(600);
+        }else{
+            $(scrollBtn).parent().stop().fadeOut(600);
+        }
+      
+        var pointHeight = $(document).height() - $('footer').outerHeight() - $(window).height();
+      
+        if(sTop > pointHeight){
+            $(scrollBtn).parent().addClass('active');
+        }else{
+            $(scrollBtn).parent().removeClass('active');
+        }
+      
+        speed = sTop / 2;
+
+        $(scrollBtn).click(function(e){
+            e.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop : 0
+        },speed);
+        });
+    });
+  });
---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var swiper = new Swiper("main .swiper", {
+        slidesPerView: 'auto', 
+        spaceBetween: 50, 
+        centeredSlides: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: "main .swiper-button-next",
+            prevEl: "main .swiper-button-prev",
+        },
+        allowTouchMove: false, 
+        speed: 800, 
+        loopedSlides: 1,
+        pagination: {
+          el: "main .swiper-pagination",
+          clickable: true,
+        },
+        loop: true,
+        
+    });
+});
+
+
+
+
